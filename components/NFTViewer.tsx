1ce5134b7418eb45dfd05403677b66c8b495500f
vice2al <vicente@oneohone.io>
2023-03-23 13:59:18 +0000

    Add nft wallet to nft view

--- components/NFTViewer.tsx
+++ components/NFTViewer.tsx
@@ -13,6 +13,7 @@ export default function NFTViewer({ nft }: any) {
       <p>Token Id: {nft.token_id}</p>
       <p>Title: {nft.metadata.title}</p>
       <p>Description: {nft.metadata.description}</p>
+      <p>Wallet: {nft.owner_id}</p>
     </div>
   );
 }
